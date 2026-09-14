460f0c4ff5dc5d55614ff7479033a8c5c0029b3b
Mirshafiee <novixweb.laravel@gmail.com>
2026-09-15 00:40:37 +0330

    add dark theme

--- js/app.js
+++ js/app.js
@@ -67,3 +67,27 @@ function scrollUp() {
 }
 window.addEventListener('scroll', scrollUp);
 
+// dark and light theme
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bi-toggle-on';
+
+// previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bi-toggle-off' : 'bi-toggle-on'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bi-toggle-off' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
